d4f00285a69f976c203602432a33a3379dc0817c
Anna Alyoshina <denielaz2020@gmail.com>
2023-04-07 13:26:59 +0300

    added a root route

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/whicencer/golang-auth/database"
@@ -21,5 +23,9 @@ func main() {
 	routes.SetupAllRoutes(app)
 
 	// Listen on host
-	app.Listen("0.0.0.0:3000")
+	log.Fatal(app.Listen("0.0.0.0:3000"))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello")
+	})
 }
